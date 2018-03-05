6e80e27b0238f24b1cb430f788973061c44e95ac
John Hay Supetran <superjohnhay@gmail.com>
2018-03-06 03:57:10 +0800

    Add files via upload

---
+++ _scripts/MoveObjects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjects : MonoBehaviour {
+
+	public GameObject items;
+	public GameObject tempParent;
+	public Transform guide;
+
+	// Use this for initialization
+	void Start () {
+		items.GetComponent<Rigidbody> ().useGravity = true;
+	}
+
+
+	void OnMouseDown()
+	{
+	
+		items.GetComponent<Rigidbody> ().useGravity = false;
+		items.GetComponent<Rigidbody> ().isKinematic = true;
+		items.transform.position = guide.transform.position;
+		items.transform.rotation = guide.transform.rotation;
+		items.transform.parent = tempParent.transform;
+	}
+
+	void OnMouseUp()
+	{
+		items.GetComponent<Rigidbody> ().useGravity = true;
+		items.GetComponent<Rigidbody> ().isKinematic = false;
+		items.transform.parent = null;
+		items.transform.position = guide.transform.position;
+
+	}
+}
---
+++ _scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Transform vrCamera;
+	public float moveSpeed;
+	private CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton (0)) {
+			Vector3 forward = vrCamera.TransformDirection (Vector3.forward);
+			cc.SimpleMove(forward * moveSpeed * Time.deltaTime);
+		}
+	}
+}
